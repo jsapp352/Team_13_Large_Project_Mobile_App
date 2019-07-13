ff64a05175a6293be22cafe96d745879a462cee3
Michael-Mignon <mmignon.michaelp@gmail.com>
2019-07-13 17:48:59 -0400

    fixed import dependencies

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import App from './styled/App';
 import {name as appName} from './app.json';
 import WaitList from './styled/WaitList';
 import React from 'react';
-import TabNavigator from './TabNavigator'
-import Student from './Student'
 import TabBar from './styled/TabBar'
 
 AppRegistry.registerComponent(appName, () => App);
